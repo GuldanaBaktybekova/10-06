6f6f4e3af7aaf1a8e7ca21ac5d1bfdd90313ef3b
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-06-10 14:36:41 +0600

    Add usestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  useEffect(function() {
+    //Read
+    setName( localStorage.getItem('name'));
+  }, [])
+ 
+  function onNameChange({ target }) {
+    //Create //Update /Delete
+    localStorage.setItem("name", target.value);
+    setName(target.value);
+  }
+  function onNameClear() {
+    localStorage.removeItem('name');
+    setName("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={name}
+        placeholder="Your name"
+        onChange={onNameChange}
+      />
+      <span>Your name is {name}</span>
+      <button onClick={onNameClear}>Clear</button>
     </div>
   );
 }
