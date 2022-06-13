1703a0e88ec61b8056d2a42f1791fe4f7198fb53
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-06-13 09:47:59 +0600

    Add 4 transform

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  // const [angle, setAngle] = useState(100);
-  // const [skew, setSkew] = useState(100);
-  // const [scale, setScale] = useState(0);
+  const [angle, setAngle] = useState(100);
+  const [skew, setSkew] = useState(100);
+  const [scale, setScale] = useState(0.5);
   const [border, setBorder] = useState(0);
 
   useEffect(function () {
     //Read
-    // setAngle(+localStorage.getItem("angle"));
-    // setSkew(+localStorage.getItem("skew"));
+    setAngle(+localStorage.getItem("angle"));
+    setSkew(+localStorage.getItem("skew"));
     setBorder(+localStorage.getItem("border"));
 
-    // setScale(+localStorage.getItem("scale"));
+    setScale(+localStorage.getItem("scale"));
   }, []);
 
-  // function onAngleChange({ target }) {
-  //   localStorage.setItem("angle", target.value);
-  //   setAngle(+target.value);
-  // }
+  function onAngleChange({ target }) {
+    localStorage.setItem("angle", target.value);
+    setAngle(+target.value);
+  }
 
-  // function onSkewChange({ target }) {
-  //   localStorage.setItem("skew", target.value);
-  //   setSkew(+target.value);
-  // }
-  // function onScaleChange({ target }) {
-  //   localStorage.setItem("scale", target.value);
-  //   setScale(+target.value);
-  // }
+  function onSkewChange({ target }) {
+    localStorage.setItem("skew", target.value);
+    setSkew(+target.value);
+  }
+  function onScaleChange({ target }) {
+    localStorage.setItem("scale", target.value);
+    setScale(+target.value);
+  }
 
-  
   function onBorderChange({ target }) {
     localStorage.setItem("border", target.value);
     setBorder(+target.value);
@@ -39,9 +38,33 @@ function App() {
     height: "200px",
     backgroundColor: "tomato",
     margin: " 50px",
-    // transform: `rotate(${angle}deg)`,
-    // transform: `skew(${skew}deg)`,
-    // transform: `scale(${scale})`,
+    transform: `rotate(${angle}deg)`,
+   
+  };
+  const styles2 = {
+    width: "200px",
+    height: "200px",
+    backgroundColor: "tomato",
+    margin: " 50px",
+   
+    transform: `skew(${skew}deg)`,
+  
+  };
+  const styles3 = {
+    width: "200px",
+    height: "200px",
+    backgroundColor: "tomato",
+    margin: " 50px",
+  
+    transform: `scale(${scale})`,
+   
+  };
+  const styles4 = {
+    width: "200px",
+    height: "200px",
+    backgroundColor: "tomato",
+    margin: " 50px",
+   
     borderRadius: `${border}%`,
   };
 
@@ -85,16 +108,35 @@ function App() {
   return (
     <div className="App">
       <div style={{ marginBottom: "100px" }}></div>
-      {/* <input type="range" min="0" max="360" value={angle} onChange={onAngleChange}  /> */}
-      {/* <input
+      <input
+        type="range"
+        min="0"
+        max="360"
+        value={angle}
+        onChange={onAngleChange}
+      />
+      <div style={styles}></div>
+      <div style={{ marginBottom: "100px" }}></div>
+      <input
         type="range"
         min="0"
         max="360"
         value={skew}
         onChange={onSkewChange}
-      /> */}
+      />
+      <div style={styles2}></div>
+      <div style={{ marginBottom: "100px" }}></div>
 
-      {/* <input type="range" min="1" max="1.5" value={scale} onChange={onScaleChange}  /> */}
+      <input
+        type="range"
+        min="1"
+        max="1.5"
+        value={scale}
+        onChange={onScaleChange} 
+        
+      />
+      <div style={styles3}></div>
+      <div style={{ marginBottom: "100px" }}></div>
       <input
         type="range"
         min="1"
@@ -102,21 +144,20 @@ function App() {
         value={border}
         onChange={onBorderChange}
       />
-
-      <div style={styles}></div>
+      <div style={styles4}></div>
 
       {/* <button onClick={onIncrease}>+</button> //2
       <p>{number}</p>
-      <button onClick={onDecrease}>- </button> */}
+      <button onClick={onDecrease}>- </button> 
 
-      {/* <input    //1
+       <input    //1
         type="text"
         value={name}
         placeholder="Your name"
         onChange={onNameChange}
       />
       <span>Your name is {name}</span>
-      <button onClick={onNameClear}>Clear</button> */}
+      <button onClick={onNameClear}>Clear</button>  */}
     </div>
   );
 }
